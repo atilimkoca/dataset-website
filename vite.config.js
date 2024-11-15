88f4e3ca536fa3ab83389a5c0eb9d30d6d6bfd83
Ömer Atılım KOCA <atilim.koca@bakircay.edu.tr>
2024-11-15 14:52:09 +0300

    add:deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base:"/dataset-website/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
